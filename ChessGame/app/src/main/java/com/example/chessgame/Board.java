e33372c1cbdaf0a1eed46a3922da3b16addefafa
XiangTGE <xiang01nij@gmail.com>
2023-11-28 00:00:05 -0500

    Made a cursory view of the menu fragment, along with button that links to board fragment ~Xiang

--- ChessGame/app/src/main/java/com/example/chessgame/Board.java
+++ ChessGame/app/src/main/java/com/example/chessgame/Board.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.chessgame.databinding.FragmentFirstBinding;
+import com.example.chessgame.databinding.BoardBinding;
 
-public class FirstFragment extends Fragment {
+public class Board extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private BoardBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = BoardBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,11 +29,11 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(Board.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }
--- ChessGame/app/src/main/java/com/example/chessgame/Menu.java
+++ ChessGame/app/src/main/java/com/example/chessgame/Menu.java
@@ -9,11 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.chessgame.databinding.FragmentSecondBinding;
+import com.example.chessgame.databinding.MenuBinding;
+        //FragmentFirstBinding;
 
-public class SecondFragment extends Fragment {
+public class Menu extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private MenuBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +22,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = MenuBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,11 +30,11 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        binding.playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(Menu.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
