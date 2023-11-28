f332053cb7257dd2148424b42d2a5557f48ff568
XiangTGE <xiang01nij@gmail.com>
2023-11-28 00:20:14 -0500

    Added chess board background, will overlay with movable pieces ~Xiang

--- ChessGame/app/src/main/java/com/example/chessgame/Board.java
+++ ChessGame/app/src/main/java/com/example/chessgame/Board.java
@@ -23,20 +23,19 @@ public class Board extends Fragment {
 
         binding = BoardBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(Board.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
-    }
+//    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(Board.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+//            }
+//        });
+//    }
 
     @Override
     public void onDestroyView() {
