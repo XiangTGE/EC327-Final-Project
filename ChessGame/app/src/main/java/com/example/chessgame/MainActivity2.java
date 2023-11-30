2a02fefbdca5e38b8a3728da7902350b9d4a02f8
XiangTGE <xiang01nij@gmail.com>
2023-11-29 20:38:01 -0500

    Setting up board pieces

--- ChessGame/app/src/main/java/com/example/chessgame/MainActivity2.java
+++ ChessGame/app/src/main/java/com/example/chessgame/MainActivity2.java
@@ -5,9 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity2 extends AppCompatActivity {
+
+    public TextView[][] BoardTiles = new TextView[8][8];
+    public TextView[][] BoardPieceSlot = new TextView[8][8];
+
+    // Function to set up the board
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
