181a19d0743aba80270c7616bfd524f9ef7b8c3d
XiangTGE <xiang01nij@gmail.com>
2023-11-30 22:07:48 -0500

    Put most of the back-end ops in GamePlay.java, use that to keep track of pieces and movement

--- ChessGame/app/src/main/java/com/example/chessgame/BoardActivity.java
+++ ChessGame/app/src/main/java/com/example/chessgame/BoardActivity.java
@@ -9,139 +9,9 @@ import android.widget.TextView;
 public class BoardActivity extends AppCompatActivity {
 
     //public TextView[][] BoardTiles = new TextView[8][8];
-    public TextView[][] BoardPieceSlots = new TextView[8][8];                                        // Stores where proper piece images should be placed
-    public Piece[][] BoardPositions = new Piece[8][8];                                              // Stores the positions of pieces
-
-
-    // Declare Piece objects
-    Piece bKing;
-    Piece wKing;
-
-    Piece bQueen;
-    Piece wQueen;
-
-    Piece bKnight1;
-    Piece bKnight2;
-    Piece wKnight1;
-    Piece wKnight2;
-
-    Piece bRook1;
-    Piece bRook2;
-    Piece wRook1;
-    Piece wRook2;
-
-    Piece bBishop1;
-    Piece bBishop2;
-    Piece wBishop1;
-    Piece wBishop2;
-
-    Piece bPawn1;
-    Piece bPawn2;
-    Piece bPawn3;
-    Piece bPawn4;
-    Piece bPawn5;
-    Piece bPawn6;
-    Piece bPawn7;
-    Piece bPawn8;
-
-    Piece wPawn1;
-    Piece wPawn2;
-    Piece wPawn3;
-    Piece wPawn4;
-    Piece wPawn5;
-    Piece wPawn6;
-    Piece wPawn7;
-    Piece wPawn8;
-
-    // Initialize the board in the background
-    private void initializeBoard () {
-
-        // Initialize Piece objects
-        bKing = new King(false);
-        wKing = new King(true);
-
-        bQueen = new Queen(false);
-        wQueen = new Queen(true);
-
-        bRook1 = new Rook(false);
-        bRook2 = new Rook(false);
-        wRook1 = new Rook(true);
-        wRook2 = new Rook(true);
-
-        bKnight1 = new Knight(false);
-        bKnight2 = new Knight(false);
-        wKnight1 = new Knight(true);
-        wKnight2 = new Knight(true);
-
-        bBishop1 = new Bishop(false);
-        bBishop2 = new Bishop(false);
-        wBishop1 = new Bishop(true);
-        wBishop2 = new Bishop(true);
-
-        bPawn1 = new Pawn(false);
-        bPawn2 = new Pawn(false);
-        bPawn3 = new Pawn(false);
-        bPawn4 = new Pawn(false);
-        bPawn5 = new Pawn(false);
-        bPawn6 = new Pawn(false);
-        bPawn7 = new Pawn(false);
-        bPawn8 = new Pawn(false);
-
-        wPawn1 = new Pawn(true);
-        wPawn2 = new Pawn(true);
-        wPawn3 = new Pawn(true);
-        wPawn4 = new Pawn(true);
-        wPawn5 = new Pawn(true);
-        wPawn6 = new Pawn(true);
-        wPawn7 = new Pawn(true);
-        wPawn8 = new Pawn(true);
-
-
-        // Store where pieces will be in starting position
-        for (int i = 0; i < 8; i++) {
-
-            for (int j = 0; j < 8; j++) {
-
-                BoardPositions[i][j] = null;
-            }
-        }
-
-        BoardPositions[0][7] = wRook1;
-        BoardPositions[1][7] = wKnight1;
-        BoardPositions[2][7] = wBishop1;
-        BoardPositions[3][7] = wQueen;
-        BoardPositions[4][7] = wKing;
-        BoardPositions[5][7] = wBishop2;
-        BoardPositions[6][7] = wKnight2;
-        BoardPositions[7][7] = wRook2;
-
-        BoardPositions[0][6] = wPawn1;
-        BoardPositions[1][6] = wPawn2;
-        BoardPositions[2][6] = wPawn3;
-        BoardPositions[3][6] = wPawn4;
-        BoardPositions[4][6] = wPawn5;
-        BoardPositions[5][6] = wPawn6;
-        BoardPositions[6][6] = wPawn7;
-        BoardPositions[7][6] = wPawn8;
-
-        BoardPositions[0][0] = bRook1;
-        BoardPositions[1][0] = bKnight1;
-        BoardPositions[2][0] = bBishop1;
-        BoardPositions[3][0] = bQueen;
-        BoardPositions[4][0] = bKing;
-        BoardPositions[5][0] = bBishop2;
-        BoardPositions[6][0] = bKnight2;
-        BoardPositions[7][0] = bRook2;
-
-        BoardPositions[0][1] = bPawn1;
-        BoardPositions[1][1] = bPawn2;
-        BoardPositions[2][1] = bPawn3;
-        BoardPositions[3][1] = bPawn4;
-        BoardPositions[4][1] = bPawn5;
-        BoardPositions[5][1] = bPawn6;
-        BoardPositions[6][1] = bPawn7;
-        BoardPositions[7][1] = bPawn8;
-    }
+    public TextView[][] BoardPieceSlots = new TextView[8][8];                                       // Stores where proper piece images should be placed
+    GamePlay game;                                                                                  // GamePlay object to keep track of game operations
+                                                                                                    // in the background
 
 
     // Set up board (UI)
@@ -223,12 +93,12 @@ public class BoardActivity extends AppCompatActivity {
 
         // Place piece images on the board, assuming that the proper piece
         // positions are already recorded in BoardPositions
-        setBoard();
+        setBoard(game.getBoardPositions());
     }
 
 
     // Refreshes the board positions based on what is in BoardPositions
-    private void setBoard() {
+    private void setBoard(Piece[][] BoardPositions) {
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -305,6 +175,18 @@ public class BoardActivity extends AppCompatActivity {
         //isKingInDanger();
     }
 
+    // Run the game, melds back-end and front-end
+    void runGame () {
+
+        // Set up the board (ONLY RUNS ONCE!!)
+        setUpBoard();
+
+
+        // Refresh board - set up board pieces
+        setBoard(game.getBoardPositions());
+    }
+
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
 
@@ -314,9 +196,10 @@ public class BoardActivity extends AppCompatActivity {
 
 
         // Set up board in background
-        initializeBoard();
+        game = new GamePlay();
 
-        // Set up board in UI
-        setUpBoard();
+
+        // Run the game
+        runGame();
     }
 }
---
+++ ChessGame/app/src/main/java/com/example/chessgame/GamePlay.java
@@ -0,0 +1,152 @@
+package com.example.chessgame;
+
+public class GamePlay {
+
+    public Piece[][] BoardPositions = new Piece[8][8];                                              // Stores the positions of pieces
+
+
+    // Declare Piece objects
+    Piece bKing;
+    Piece wKing;
+
+    Piece bQueen;
+    Piece wQueen;
+
+    Piece bKnight1;
+    Piece bKnight2;
+    Piece wKnight1;
+    Piece wKnight2;
+
+    Piece bRook1;
+    Piece bRook2;
+    Piece wRook1;
+    Piece wRook2;
+
+    Piece bBishop1;
+    Piece bBishop2;
+    Piece wBishop1;
+    Piece wBishop2;
+
+    Piece bPawn1;
+    Piece bPawn2;
+    Piece bPawn3;
+    Piece bPawn4;
+    Piece bPawn5;
+    Piece bPawn6;
+    Piece bPawn7;
+    Piece bPawn8;
+
+    Piece wPawn1;
+    Piece wPawn2;
+    Piece wPawn3;
+    Piece wPawn4;
+    Piece wPawn5;
+    Piece wPawn6;
+    Piece wPawn7;
+    Piece wPawn8;
+
+
+    // Set up board, start the game
+    public GamePlay () {
+
+        initializeBoard();
+    }
+
+
+    // Initialize the board in the background
+    private void initializeBoard () {
+
+        // Initialize Piece objects
+        bKing = new King(false);
+        wKing = new King(true);
+
+        bQueen = new Queen(false);
+        wQueen = new Queen(true);
+
+        bRook1 = new Rook(false);
+        bRook2 = new Rook(false);
+        wRook1 = new Rook(true);
+        wRook2 = new Rook(true);
+
+        bKnight1 = new Knight(false);
+        bKnight2 = new Knight(false);
+        wKnight1 = new Knight(true);
+        wKnight2 = new Knight(true);
+
+        bBishop1 = new Bishop(false);
+        bBishop2 = new Bishop(false);
+        wBishop1 = new Bishop(true);
+        wBishop2 = new Bishop(true);
+
+        bPawn1 = new Pawn(false);
+        bPawn2 = new Pawn(false);
+        bPawn3 = new Pawn(false);
+        bPawn4 = new Pawn(false);
+        bPawn5 = new Pawn(false);
+        bPawn6 = new Pawn(false);
+        bPawn7 = new Pawn(false);
+        bPawn8 = new Pawn(false);
+
+        wPawn1 = new Pawn(true);
+        wPawn2 = new Pawn(true);
+        wPawn3 = new Pawn(true);
+        wPawn4 = new Pawn(true);
+        wPawn5 = new Pawn(true);
+        wPawn6 = new Pawn(true);
+        wPawn7 = new Pawn(true);
+        wPawn8 = new Pawn(true);
+
+
+        // Store where pieces will be in starting position
+        for (int i = 0; i < 8; i++) {
+
+            for (int j = 0; j < 8; j++) {
+
+                BoardPositions[i][j] = null;
+            }
+        }
+
+        BoardPositions[0][7] = wRook1;
+        BoardPositions[1][7] = wKnight1;
+        BoardPositions[2][7] = wBishop1;
+        BoardPositions[3][7] = wQueen;
+        BoardPositions[4][7] = wKing;
+        BoardPositions[5][7] = wBishop2;
+        BoardPositions[6][7] = wKnight2;
+        BoardPositions[7][7] = wRook2;
+
+        BoardPositions[0][6] = wPawn1;
+        BoardPositions[1][6] = wPawn2;
+        BoardPositions[2][6] = wPawn3;
+        BoardPositions[3][6] = wPawn4;
+        BoardPositions[4][6] = wPawn5;
+        BoardPositions[5][6] = wPawn6;
+        BoardPositions[6][6] = wPawn7;
+        BoardPositions[7][6] = wPawn8;
+
+        BoardPositions[0][0] = bRook1;
+        BoardPositions[1][0] = bKnight1;
+        BoardPositions[2][0] = bBishop1;
+        BoardPositions[3][0] = bQueen;
+        BoardPositions[4][0] = bKing;
+        BoardPositions[5][0] = bBishop2;
+        BoardPositions[6][0] = bKnight2;
+        BoardPositions[7][0] = bRook2;
+
+        BoardPositions[0][1] = bPawn1;
+        BoardPositions[1][1] = bPawn2;
+        BoardPositions[2][1] = bPawn3;
+        BoardPositions[3][1] = bPawn4;
+        BoardPositions[4][1] = bPawn5;
+        BoardPositions[5][1] = bPawn6;
+        BoardPositions[6][1] = bPawn7;
+        BoardPositions[7][1] = bPawn8;
+    }
+
+
+    // Give access to BoardPositions
+    public Piece[][] getBoardPositions () {
+
+        return BoardPositions;
+    }
+}
